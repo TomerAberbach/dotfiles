e28a3bfd3d275d990e0e121d18e5ea7bac13c68e
Tomer Aberbach <tomeraberbach@gmail.com>
2020-01-31 20:21:58 -0500

    feat: add support for direnv

--- home/tomeraberbach/.config/fish/config.fish
+++ home/tomeraberbach/.config/fish/config.fish
@@ -1,6 +1,9 @@
 # Ensure single instances of ssh- and gpg-agent
 eval (keychain --agents ssh,gpg --quiet --eval)
 
+# Configure direnv (https://direnv.net)
+direnv hook fish | source
+
 # Configure Starship Prompt (https://starship.rs)
 starship init fish | source
 
