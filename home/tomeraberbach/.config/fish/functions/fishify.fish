281d9701a5f0131dfc229003e7251d85e4ac27a4
Tomer Aberbach <tomeraberbach@gmail.com>
2020-02-01 16:43:50 -0500

    feat: fishify -q for quoting

--- home/tomeraberbach/.config/fish/functions/fishify.fish
+++ home/tomeraberbach/.config/fish/functions/fishify.fish
@@ -1,6 +1,9 @@
 function fishify
   if fishy $argv[1]
     set argv fish -c "$argv"
+  else if test $argv[1] = '-q'; and fishy $argv[2]
+    set -e argv[1]
+    set argv fish -c "\"$argv\""
   end
 
   array $argv
