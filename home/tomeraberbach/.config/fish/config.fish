0dd5c2d48dd3fc5a71ff5fa025473906856b5d08
Tomer Aberbach <tomeraberbach@gmail.com>
2020-02-10 12:52:17 -0500

    feat: support mkcert for node

--- home/tomeraberbach/.config/fish/config.fish
+++ home/tomeraberbach/.config/fish/config.fish
@@ -1,6 +1,9 @@
 # Ensure single instances of ssh- and gpg-agent
 eval (keychain --agents ssh,gpg --quiet --eval)
 
+# Configure NodeJS with mkcert (https://github.com/FiloSottile/mkcert)
+set -x NODE_EXTRA_CA_CERTS (mkcert -CAROOT)/rootCA.pem
+
 # Configure direnv (https://direnv.net)
 direnv hook fish | source
 
