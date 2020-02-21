218ebdc7eb9d1acd49cc12e3f5a337f988d210e0
Tomer Aberbach <tomeraberbach@gmail.com>
2020-02-21 14:06:37 -0500

    feat: switch to pure prompt

--- home/tomeraberbach/.config/fish/config.fish
+++ home/tomeraberbach/.config/fish/config.fish
@@ -7,6 +7,3 @@ set -x NODE_EXTRA_CA_CERTS (mkcert -CAROOT)/rootCA.pem
 # Configure direnv (https://direnv.net)
 direnv hook fish | source
 
-# Configure Starship Prompt (https://starship.rs)
-starship init fish | source
-
