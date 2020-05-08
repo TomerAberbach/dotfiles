95adc0ec8a975612ed461c88e667d25d213abeea
Tomer Aberbach <tomeraberbach@gmail.com>
2020-05-08 19:48:38 -0400

    feat: update fish config

--- home/tomeraberbach/.config/fish/config.fish
+++ home/tomeraberbach/.config/fish/config.fish
@@ -7,3 +7,7 @@ set -x NODE_EXTRA_CA_CERTS (mkcert -CAROOT)/rootCA.pem
 # Configure direnv (https://direnv.net)
 direnv hook fish | source
 
+
+# tabtab source for packages
+# uninstall by removing these lines
+[ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
