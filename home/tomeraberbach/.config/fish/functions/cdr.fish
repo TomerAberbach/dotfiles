0b053947c5ded12bbb173cd79af962723584a4e6
Tomer Aberbach <tomeraberbach@gmail.com>
2020-06-04 19:26:48 -0400

    fix: make cdr exit code 1 on fail

--- home/tomeraberbach/.config/fish/functions/cdr.fish
+++ home/tomeraberbach/.config/fish/functions/cdr.fish
@@ -1,5 +1,10 @@
 function cdr
-  set -l root (git rev-parse --show-toplevel)
-  and cd $root
+  set -l git_dir (find_up '.git')
+
+  if test -z $git_dir
+    return 1
+  end
+
+  cd (dirname $git_dir)
 end
 
